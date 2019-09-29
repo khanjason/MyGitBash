719fb59375c686013d7fe02810c617ec56271fa8
khanjason <jasonxdecember@gmail.com>
2019-09-29 01:09:22 +0100

    changes to timekeeping

--- timekeeper.sh
+++ timekeeper.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
-input="reminders.txt"
-while true; do
-while IFS=' ' read -r line
-do
-  #echo "line"
-  #echo "$line"
-  current=$(date +%d)
-  current+="/"
-  current+=$(date +%m)
-  current+="/"
-  current+=$(date +%Y)
-  current+=" "
-  current+=$(date +%H)
-  current+=":"
-  current+=$(date +%M)
-  current+=" "
-
+if [ -f "reminders.txt" ]; then	
+	input="reminders.txt"
+	while true; do
+	while IFS=' ' read -r line
+	do
+  		#echo "line"
+  		#echo "$line"
+  		current=$(date +%d)
+  		current+="/"
+  		current+=$(date +%m)
+  		current+="/"
+  		current+=$(date +%Y)
+  		current+=" "
+  		current+=$(date +%H)
+  		current+=":"
+  		current+=$(date +%M)
+  		current+=" "
+                
+		
   
-  lincut=${line%|*}
+  		lincut=${line%|*}
    
-  if  [ "$current" == "$lincut" ]; then
-        echo "REMINDER"
-        echo "Reminder : $line"
-  fi	
+  		if  [ "$current" == "$lincut" ] ; 
+then
+ 			
+	       		echo "REMINDER"
+        		echo "Reminder : ${line##*|}"
+        		sleep 1m
+			  
+  		fi	
   
-done < "$input"
-done
+	done < "$input"
+	done
+
+fi
